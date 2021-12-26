67cf1fa70456f7b5c24aa3ac840d4c70fd6a5c9d
Aayush Soni <96689777+aayushsoni19@users.noreply.github.com>
2021-12-26 19:59:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,3 @@ pet_name = input("What's the name of your pet?\n")
 #Combine the name of their city and pet and show their band name.
 band_name = (user_input + " " + pet_name)
 print("Your band name could be " ,band_name)
-
-#5. Make sure the input cursor shows on a new line, see the example at:
-#   https://band-name-generator-end.appbrewery.repl.run/
